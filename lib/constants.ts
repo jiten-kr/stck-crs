65ca29c55e24caa29072759d982708fdd6f802e6
Jitendra Kumar <jkumar.c@cylinderhealth.com>
2026-03-09 19:59:31 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,5 +10,5 @@ export const PLATFORM_SUPPORT_PHONE = "+91 9997336099";
 export const GENERAL_SUPPORT_EMAIL = "mayankfinhub@gmail.com";
 export const LEARNERS_COUNT = "2,000+";
 export const LIVE_TRADING_CLASS_ITEM_ID = 2;
-export const LIVE_TRADING_CLASS_PRICE_INR = 1;
+export const LIVE_TRADING_CLASS_PRICE_INR = 59;
 export const LIVE_TRADING_CLASS_NAME = "Live Trading Class";
