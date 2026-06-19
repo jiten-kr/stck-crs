386142ea840fcb68a03fd616d5c0a8522a0f8bf8
Jitendra Kumar <jkumar.c@cylinderhealth.com>
2026-06-19 10:23:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import PixelTracker from "@/components/PixelTracker"
 import "./globals.css"
 import { Providers } from "@/components/providers"
 
-const META_PIXEL_ID = "988231623817770";
+const META_PIXEL_ID = "987766333967027";
 
 
 export default function RootLayout({
